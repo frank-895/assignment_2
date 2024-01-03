5d1d685ec4e9199c06334102a52e114d8ed8aaf0
Frank Snelling <frank.snelling03@gmail.com>
2024-01-03 16:35:58 +0000

    question 1 part 4 started

---
+++ q1_part4.py
@@ -0,0 +1,25 @@
+import spacy
+import scispacy
+
+model1 = spacy.load("en_core_sci_sm")
+model2 = spacy.load("en_ner_bc5cdr_md")
+
+word_list_1 = []
+word_list_2 = []
+
+with open('new_file.txt', 'r') as file1:
+    long_string = file1.read()
+    long_string = long_string.splitlines()
+    for section in long_string:
+      
+      temp = model1(section)
+      for i in temp.ents:
+        if i not in word_list_1:
+          word_list_1.append(i)
+      temp = model2(section)
+      for i in temp.ents:
+        if i not in word_list_1:
+          word_list_1.append(i)
+
+print(len(word_list_1))
+print(word_list_1)
